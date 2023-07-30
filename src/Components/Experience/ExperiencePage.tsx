f6aa3d8403047ea458f771f0e5eeea059a0c7da8
M-Hassan1 <hassan81932@gmail.com>
2023-07-31 01:17:11 +0500

    Added some feature

--- src/Components/Experience/ExperiencePage.tsx
+++ src/Components/Experience/ExperiencePage.tsx
@@ -2,67 +2,197 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 
-import Working from '@/Components/Assets/Images/pic-0.png'
+import website from '@/Components/Assets/Images/e-commerce.png'
+import moringa from '@/Components/Assets/Images/moringa-powder.png'
 import Webone from '@/Components/Assets/Images/pic-1.png'
-import Twoweb from '@/Components/Assets/Images/pic-2.png'
-import Threeweb from '@/Components/Assets/Images/pic-3.png'
+import weatherApp from '@/Components/Assets/Images/weather-app.png'
+import Working from '@/Components/Assets/Images/pic-0.png'
+import sideBar from '@/Components/Assets/Images/SideBar.png'
 import Webfour from '@/Components/Assets/Images/pic-4.png'
 import Fiveweb from '@/Components/Assets/Images/pic-5.png'
+import portfolio from '@/Components/Assets/Images/portfolio website.png'
 
 const ExperiencePage = () => {
   return (
     <div className='project' >
-      <div className='pt-32 mt-52  '>
+      <div className='pt-60'>
         <h1 className='p-4 text-4xl text-[#3C82F6] '>Experience</h1>
         <h2 className='p-4 text-2xl text-gray-500 leading-none'>
           The latest project I have created
           </h2>
           <div className='p-4 grid gap-8 lg:grid-cols-2 md:grid-cols-2'>
             <div id='row' className='row'>
-            {/* <div className='img' >my image hare</div> */}
-            <Image src={Working} alt='MyImage' />
+            <Image src={website} alt='MyImage' /> 
               <div className='layer'>
-                <h5>Ux and Ui Design</h5>
+                <h5>E Commerce Websites</h5>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor fugit labore nemo</p>
-                <Link href='/' className='bx bx-link' >icons</Link>
+                <div className='flex flex-row gap-4' >
+                <Link href='https://e-commerce-pink-three.vercel.app' >
+                  <button className='bg-blue-500 hover:bg-blue-400 p-2 rounded-md'>
+                    Demo
+                  </button>
+                  </Link>
+                  <Link href='/' >
+                  <button className='bg-blue-500 hover:bg-blue-400 p-2 rounded-md'>
+                    Code
+                  </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div id='row' className='row'>
+            <Image src={moringa} alt='MyImage' />
+              <div className='layer'>
+                <h5>Moringa Powder Website</h5>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor fugit labore nemo</p>
+                <div className='flex flex-row gap-4' >
+                <Link href='https://moringa-powder.vercel.app'>
+                  <button className='bg-blue-500 hover:bg-blue-400 p-2 rounded-md'>
+                    Demo
+                  </button>
+                  </Link>
+                  <Link href='/'>
+                  <button className='bg-blue-500 hover:bg-blue-400 p-2 rounded-md'>
+                    Code
+                  </button>
+                  </Link>
+                </div>
               </div>
             </div>
             <div id='row' className='row'>
             {/* <div className='img' >my image hare</div> */}
-            <Image src={Working} alt='MyImage' />
+            <Image src={weatherApp} alt='MyImage' />
               <div className='layer'>
-                <h5>Ux and Ui Design</h5>
+                <h5>Ui and Ux Design</h5>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor fugit labore nemo</p>
-                <Link href='/' className='bx bx-link' >icons</Link>
+                <div className='flex flex-row gap-4' >
+                <Link href='/' >
+                  <button className='bg-blue-500 hover:bg-blue-400 p-2 rounded-md'>
+                    Demo
+                  </button>
+                  </Link>
+                  <Link href='/' >
+                  <button className='bg-blue-500 hover:bg-blue-400 p-2 rounded-md'>
+                    Code
+                  </button>
+                  </Link>
+                </div>
               </div>
             </div>
             <div id='row' className='row'>
-            {/* <div className='img' >my image hare</div> */}
-            <Image src={Working} alt='MyImage' />
+            <Image src={Webone} alt='MyImage' />
               <div className='layer'>
-                <h5>Ux and Ui Design</h5>
+                <h5>Ui and Ux Design</h5>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor fugit labore nemo</p>
-                <Link href='/' className='bx bx-link' >icons</Link>
+                <div className='flex flex-row gap-4' >
+                <Link href='/' >
+                  <button className='bg-blue-500 hover:bg-blue-400 p-2 rounded-md'>
+                    Demo
+                  </button>
+                  </Link>
+                  <Link href='/' >
+                  <button className='bg-blue-500 hover:bg-blue-400 p-2 rounded-md'>
+                    Code
+                  </button>
+                  </Link>
+                </div>
               </div>
             </div>
             <div id='row' className='row'>
-            {/* <div className='img' >my image hare</div> */}
             <Image src={Working} alt='MyImage' />
               <div className='layer'>
-                <h5>Ux and Ui Design</h5>
+                <h5>Ui and Ux Design</h5>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor fugit labore nemo</p>
-                <Link href='/' className='bx bx-link' >icons</Link>
+                <div className='flex flex-row gap-4' >
+                <Link href='/' >
+                  <button className='bg-blue-500 hover:bg-blue-400 p-2 rounded-md'>
+                    Demo
+                  </button>
+                  </Link>
+                  <Link href='/' >
+                  <button className='bg-blue-500 hover:bg-blue-400 p-2 rounded-md'>
+                    Code
+                  </button>
+                  </Link>
+                </div>
               </div>
             </div>
             <div id='row' className='row'>
-              {/* <div className='img' >my image hare</div> */}
-            <Image src={Working} alt='MyImage' />
+            <Image src={sideBar} alt='MyImage' />
               <div className='layer'>
-                <h5>Ux and Ui Design</h5>
+                <h5>Ui and Ux Design</h5>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor fugit labore nemo</p>
-                <Link href='/' className='bx bx-link' >icons</Link>
+                <div className='flex flex-row gap-4' >
+                <Link href='/' >
+                  <button className='bg-blue-500 hover:bg-blue-400 p-2 rounded-md'>
+                    Demo
+                  </button>
+                  </Link>
+                  <Link href='/' >
+                  <button className='bg-blue-500 hover:bg-blue-400 p-2 rounded-md'>
+                    Code
+                  </button>
+                  </Link>
+                </div>
+              </div>
+            </div>   
+            <div id='row' className='row'>
+            <Image src={Fiveweb} alt='MyImage' />
+              <div className='layer'>
+                <h5>Ui and Ux Design</h5>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor fugit labore nemo</p>
+                <div className='flex flex-row gap-4' >
+                <Link href='/' >
+                  <button className='bg-blue-500 hover:bg-blue-400 p-2 rounded-md'>
+                    Demo
+                  </button>
+                  </Link>
+                  <Link href='/' >
+                  <button className='bg-blue-500 hover:bg-blue-400 p-2 rounded-md'>
+                    Code
+                  </button>
+                  </Link>
+                </div>
               </div>
             </div>
+            <div id='row' className='row'>
+            <Image src={portfolio} alt='MyImage' />
+              <div className='layer'>
+                <h5>Ui and Ux Design</h5>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor fugit labore nemo</p>
+                <div className='flex flex-row gap-4' >
+                <Link href='/' >
+                  <button className='bg-blue-500 hover:bg-blue-400 p-2 rounded-md'>
+                    Demo
+                  </button>
+                  </Link>
+                  <Link href='/' >
+                  <button className='bg-blue-500 hover:bg-blue-400 p-2 rounded-md'>
+                    Code
+                  </button>
+                  </Link>
+                </div>
+              </div>
+            </div>   
+            <div id='row' className='row'>
+            <Image src={Webfour} alt='MyImage' />
+              <div className='layer'>
+                <h5>Ui and Ux Design</h5>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor fugit labore nemo</p>
+                <div className='flex flex-row gap-4' >
+                <Link href='/' >
+                  <button className='bg-blue-500 hover:bg-blue-400 p-2 rounded-md'>
+                    Demo
+                  </button>
+                  </Link>
+                  <Link href='/' >
+                  <button className='bg-blue-500 hover:bg-blue-400 p-2 rounded-md'>
+                    Code
+                  </button>
+                  </Link>
+                </div>
+              </div>
+            </div>  
           </div>
       </div>
 
@@ -70,42 +200,4 @@ const ExperiencePage = () => {
   )
 }
 
-export default ExperiencePage
-
-
-
-
-
-// import Link from 'next/link';
-// import React from 'react';
-
-// const ExperiencePage = () => {
-//   return (
-//     <div className='project'>
-//       <div className='pt-32 mt-52'>
-//         <h2 className='text-4xl leading-none text-center'>
-//           The latest project I have created
-//         </h2>
-//         <div className='grid gap-8 lg:grid-cols-[minmax(350px,auto),auto]'>
-//           {/* First grid item */}
-//           <div className='row'>
-//             <div className='img'>my image here</div>
-//             <div className='layer'>
-//               <h5>Ux and Ui Design</h5>
-//               <p>
-//                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor fugit labore nemo
-//               </p>
-//               <Link href='/' className='bx bx-link'>
-//                 icons
-//               </Link>
-//             </div>
-//           </div>
-
-//           {/* Add more grid items as needed */}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ExperiencePage;
+export default ExperiencePage;
