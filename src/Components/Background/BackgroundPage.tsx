23b89be747a2996d94dc0dfd71b6155b8260118d
M-Hassan1 <hassan81932@gmail.com>
2023-09-30 10:39:23 +0500

    Code Msked More Clean

--- src/Components/Background/BackgroundPage.tsx
+++ src/Components/Background/BackgroundPage.tsx
@@ -2,21 +2,25 @@ import React from 'react'
 
 const BackgroundPage = () => {
   return (
- 
-  <div className="area bg-gradient-to-r from-[#323232] to-[#313865] w-full h-screen absolute z-[-1]">
-  <ul className="circles absolute top-0 left-0 w-full h-[96%] overflow-hidden">
-    <li className="absolute list-none w-20 h-20 bg-opacity-20 bg-white animate-rotate-bottom-25 bottom-[-150px] left-[25%] delay-0"></li>
-    <li className="absolute list-none w-20 h-20 bg-opacity-20 bg-white animate-rotate-bottom-25 bottom-[-150px] left-[10%] delay-200 duration-12000"></li>
-    <li className="absolute list-none w-20 h-20 bg-opacity-20 bg-white animate-rotate-bottom-25 bottom-[-150px] left-[70%] delay-400"></li>
-    <li className="absolute list-none w-60 h-60 bg-opacity-20 bg-white animate-rotate-bottom-25 bottom-[-150px] left-[40%] delay-0 duration-18000"></li>
-    <li className="absolute list-none w-20 h-20 bg-opacity-20 bg-white animate-rotate-bottom-25 bottom-[-150px] left-[65%] delay-0"></li>
-    <li className="absolute list-none w-110 h-110 bg-opacity-20 bg-white animate-rotate-bottom-25 bottom-[-150px] left-[75%] delay-300"></li>
-    <li className="absolute list-none w-150 h-150 bg-opacity-20 bg-white animate-rotate-bottom-25 bottom-[-150px] left-[35%] delay-700"></li>
-    <li className="absolute list-none w-25 h-25 bg-opacity-20 bg-white animate-rotate-bottom-25 bottom-[-150px] left-[50%] delay-1500 duration-45000"></li>
-    <li className="absolute list-none w-15 h-15 bg-opacity-20 bg-white animate-rotate-bottom-25 bottom-[-150px] left-[20%] delay-200 duration-35000"></li>
-    <li className="absolute list-none w-150 h-150 bg-opacity-20 bg-white animate-rotate-bottom-25 bottom-[-150px] left-[85%] delay-0 duration-11000"></li>
-  </ul>
+
+
+<div className="area">
+<ul className="circles">
+  <li className='one'></li>
+  <li className='two'></li>
+  <li className='three'></li>
+  <li className='four'></li>
+  <li className='five'></li>
+  <li className='six'></li>
+  <li className='seven'></li>
+  <li className='eight'></li>
+  <li className='nine'></li>
+  <li className='ten'></li>
+</ul>
 </div>
+
+
+ 
 
   )
 }
--- src/Components/Home/HomePage.tsx
+++ src/Components/Home/HomePage.tsx
@@ -1,10 +1,10 @@
 "use client"
 import { useEffect, useRef } from 'react';
-import { AiOutlineMail } from 'react-icons/ai';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { BsYoutube ,BsFacebook } from 'react-icons/bs';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import Typed from 'typed.js';
 import BackgroundPage from '../Background/BackgroundPage';
+import Link from 'next/link';
 
 const HomePage = () => {
   const typedRef = useRef(null);
@@ -31,7 +31,7 @@ const HomePage = () => {
       <div className='absolute top-0 left-0 right-0 bottom-0'>
         <BackgroundPage />
       </div>
-      <div className='w-full h-screen relative font-oswald pt-8 md:pt-46 text-center'>
+      <div className='w-full h-screen relative font-oswald pt-8 text-center'>
         <div className='max-w-screen-xl w-full h-full mx-auto p-4 flex flex-col justify-center items-center'>
           <div>
             <div className='py-2 font-bold font-urbanist text-3xl md:text-5xl text-gray-200'>
@@ -41,35 +41,44 @@ const HomePage = () => {
               <span ref={typedRef}></span>
             </p>
             <div>
-              <p className='py-4 max-w-2xl mx-auto'>
+              <p className='py-4 max-w-2xl mx-auto text-slate-300 '>
                 My Name Is Muhammad-Hassan. I am a Web-Developer, Youtuber, Graphic Designer
               </p>
             </div>
+            
             <div className='flex items-center justify-center space-x-5 md:space-x-6 py-4'>
+            <Link href="/">
               <div
                 title='Linkedin Account'
                 className='rounded-full shadow-[0px_1px_1px_2px_rgba(0,0,0,0.4)] shadow-gray-300 p-4 cursor-pointer hover:scale-[1.2] ease-in duration-300'
               >
-                <FaLinkedin />
+                <FaLinkedin className="text-white"/>
               </div>
+              </Link>
+              <Link href="/">
               <div
                 title='Github Account'
                 className='rounded-full shadow-[0px_1px_1px_2px_rgba(0,0,0,0.3)] shadow-gray-300 p-4 cursor-pointer hover:scale-[1.2] ease-in duration-300'
               >
-                <FaGithub />
+                <FaGithub className="text-white" />
               </div>
-              <div
+              </Link>
+              <Link href="/">
+                <div
                 title='Send Massage On Gmail'
                 className='rounded-full shadow-[0px_1px_1px_2px_rgba(0,0,0,0.3)] shadow-gray-300 p-4 cursor-pointer hover:scale-[1.2] ease-in duration-300 '
               >
-                <AiOutlineMail />
+                <BsYoutube className="text-white" />
               </div>
+              </Link>
+              <Link href="/">
               <div
                 title='Personal Link'
                 className='rounded-full shadow-[0px_1px_1px_2px_rgba(0,0,0,0.3)] shadow-gray-300 p-4 cursor-pointer hover:scale-[1.2] ease-in duration-300'
               >
-                <BsFillPersonLinesFill  />
+                <BsFacebook className="text-white" />
               </div>
+              </Link>
             </div>
           </div>
         </div>
