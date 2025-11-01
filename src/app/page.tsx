d3a117834007923f08800f04fc05fea8481438bd
mhassan35 <mhassan909234@gmail.com>
2025-11-01 18:34:35 +0500

    removed dummy data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
-import ScrollTopBtn from "@/Components/shared/ScrollTop/ScrollBar"
-
 import HomePage from "./page/Home/page"
 import ExperiencePage from "./page/Experience/page"
 import AboutPage from "./page/About/page"
 import ContactPage from "./page/FormValidate/page"
-import SkillsCode from "./page/Skills/page"
+import SkillsPage from "./page/Skills/page"
 
 export default function Home() {
   return (
    <main>
     <HomePage />
-    <SkillsCode />
+    <SkillsPage />
     <ExperiencePage />
     <AboutPage />
     <ContactPage />
-    <ScrollTopBtn />
     </main>
   )
 }
